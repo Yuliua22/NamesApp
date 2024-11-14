1f053b9b0f728bbe47aeb02bc2adbac1c8085d1b
Yulia Prokopeva <uliaprokopeva2@gmail.com>
2024-11-14 17:21:17 +0300

    Initial Commit

---
+++ NamesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesApp
+//
+//  Created by Юлия Прокопьева on 11/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
